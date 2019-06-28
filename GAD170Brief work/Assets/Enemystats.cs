09fb9cd49946433b16e6a012c38ae44ada88c0db
Rohith Maddali <1015083@student.sae.edu.au>
2019-06-28 19:55:43 +1000

    Created a new repository for the project and moved all my brief script into this project

---
+++ GAD170Brief work/Assets/Enemystats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemystats : MonoBehaviour
+{
+    public int health = 120;
+    public int Dmg = 5;
+}
---
+++ GAD170Brief work/Assets/Playerstats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Playerstats : MonoBehaviour
+{
+    public int level = 0;
+    public int Health = 100;
+    public float Defense = 0;
+    public int Dmg = 50;
+    public int Speed = 200;
+    public int exp = 0;
+    public enum Skills
+    {
+        Poison,
+        Flame,
+        Ice,
+        Crit,
+        DoubleAtt,
+    }
+   //These are the initial player stats when he startes the game and is at level 1
+}
